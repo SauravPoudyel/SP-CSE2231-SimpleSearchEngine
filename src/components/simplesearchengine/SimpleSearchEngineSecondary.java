42db682c2b7866605ae333a9273a114f5b206be8
Saurav Poudyel <sauravpoudyel@gmail.com>
2024-11-07 12:02:45 -0500

    deleted random file

--- src/components/simplesearchengine/SimpleSearchEngineSecondary.java
+++ src/components/simplesearchengine/SimpleSearchEngineSecondary.java
@@ -4,12 +4,14 @@ package components.simplesearchengine;
  * {@code SimpleSearchEngineKernel<T>} enhanced with secondary methods.
  *
  * @param <T>
- *                type of {@code SimpleSearchEngine} entries
+ *            type of {@code SimpleSearchEngine} entries
  *
  * @author Saurav Poudyel
  *
  */
 public abstract class SimpleSearchEngineSecondary<T>
-                implements SimpleSearchEngine<T> {
+        implements SimpleSearchEngine<T> {
+
+
 
 }
